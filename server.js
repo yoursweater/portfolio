4a211b573a788b46f19935fb721723d8c92dd055
Dan Eversen <daneversen@gmail.com>
2016-06-29 11:44:54 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+///////////////////////////Setup
+
+var express = require('express');
+var app = express();
+var request = require('request');
+var bodyParser = require('body-parser');
+var mustacheExpress = require('mustache-express')
+var exphbs  = require('express-handlebars');
+var pry = require('pryjs')
+var async = require('async')
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname+'/public'))
+app.use('/results', express.static('public'));
+
+app.use(bodyParser.text());
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+var port = process.env.PORT || 8000
+var server = app.listen(port);
+console.log('Listening on port ' + port);
+
+
+//////////////////////Server
+
+app.get('/', function(req,res){
+  res.render('index')
+})
