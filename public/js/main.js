46b4fe39d29a5c0516a2450d334dd8da24918db9
Dan Eversen <daneversen@gmail.com>
2016-07-11 16:52:32 -0400

    add jquery box shadows

--- public/js/main.js
+++ public/js/main.js
@@ -3,4 +3,11 @@
 
   console.log('script loaded')
 
+  $('.card.blue-grey.darken-1').hover(function(){
+    console.log('mouse noticed')
+    $(this).css("box-shadow", "5px 5px 5px 5px #ccc")
+  }, function(){
+    $(this).css("box-shadow", "0px 0px 0px 0px #ccc")
+  })
+
 })();
