5b7dca16b557ec5134653eeb67ce982ea37207f1
Dan Eversen <daneversen@gmail.com>
2016-12-02 17:49:14 -0500

    add show/hide functionality to navbar

--- public/js/main.js
+++ public/js/main.js
@@ -3,9 +3,11 @@
 
   console.log('script loaded')
 
-$(document).ready(function(){
-  $('.slider').slider();
-});
+//Materialize
+
+  $(document).ready(function(){
+    $('.slider').slider();
+  });
 
   $('.card.blue-grey.darken-1').hover(function(){
     console.log('mouse noticed')
@@ -36,5 +38,48 @@ $(document).ready(function(){
     } }
   ];
   Materialize.scrollFire(options);
+
+//Navbar
+
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('#navMenu').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+
+
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('#navMenu').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('#navMenu').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+
+    lastScrollTop = st;
+}
+
+
 
 })();
