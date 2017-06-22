973076b03a7012ebbf0ec8ff5ecea9a8e82b317c
Dan Eversen <daneversen@gmail.com>
2017-06-22 13:43:48 -0400

    new intro

--- public/js/main.js
+++ public/js/main.js
@@ -103,4 +103,16 @@ $('#linkIcon').hover(function(){
     });
 
 
+//INTRO
+
+ var a = $(".text");
+    var b = $(".hr");
+    var c = $(".descText");
+
+    a.delay(1000).animate({top: "0", opacity: "1"}, 1000, function() {
+        b.delay(500).animate({marginLeft: "0"}, 800, function() {
+            c.animate({marginTop: "10"}, 800)
+        });
+    });
+
 })();
